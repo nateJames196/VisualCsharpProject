e6eb1a4f3d34bd3bb4658501fd2360a5d9fa2f7c
Nathan James <masterygreentea@gmail.com>
2020-04-09 13:25:25 -0400

    Solution folder

---
+++ MortgageRateCalculator/UserInformation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MortgageRateCalculator {
+    class UserInformation {
+        public string address { get; set; }
+        public string name { get; set; }
+        public string socialSecurity { get; set; }
+        public int creditScore { get; set; }
+        public double amountOfLoan { get; set; }
+        public int lengthOfLoan { get; set; }
+        public string typeOfLoan { get; set; }
+        public double income { get; set; }
+        public double mortgagePayment { get; set; }
+
+        public UserInformation(string address, string name, string social, int creditS, double amountL, int length, string type, double income) {
+            this.address = address;
+            this.name = name;
+            this.socialSecurity = social;
+            this.creditScore = creditS;
+            this.amountOfLoan = amountL;
+            this.lengthOfLoan = length;
+            this.typeOfLoan = type;
+            this.income = income;
+        }
+    }
+}
